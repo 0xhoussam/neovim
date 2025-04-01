780fe1aab12e6878abbc886460da56843343bd8e
0xhoussam <owner@0xhoussam.me>
2025-04-01 16:16:15 +0000

    feat(lsp): add lua settings

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -3,4 +3,12 @@ return {
 	root_markers = { "init.lua", ".git" },
 	filetypes = { "lua" },
 	single_file_support = true,
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+		},
+	},
 }
